cd171fd88bd1d24b8de8e6066d33568a691510e4
dmosc <osdavrm@gmail.com>
2022-05-03 23:32:04 -0500

    Add database data upload and /categories endpoint.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+const express = require("express");
+const cors = require("cors");
+const {loadDDLQueries, getDatabase} = require("./database");
+
+loadDDLQueries();
+
+const app = express();
+app.use(cors());
+
+app.get('/categories', (_, res) => {
+  const database = getDatabase();
+  database.query('SELECT * FROM category', (error, rows) => {
+    if (error) res.status(400).json(error);
+    res.status(200).json(rows);
+  });
+});
+
+app.listen(4000, () => console.log('Listening on port 4000'));
